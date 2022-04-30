907a363580ca6829f842c9df1fc60b0ee3357c71
163114 <88505337+163114@users.noreply.github.com>
2022-04-30 23:15:17 +0300

    Final push for today

--- s21_string+/src/s21_sprintf.c
+++ s21_string+/src/s21_sprintf.c
@@ -21,12 +21,27 @@ Task - Implement the sprintf function from the stdio.h library
 // Include the the char array of specifiers
 #include "s21_specifiers.h"
 
+/*
+#define function1(type,argp) \
+{ \
+if (type == 'd') \
+    return va_arg(argp, int); \
+if (type == 'c') \
+    return va_arg(argp, char); \
+}
+*/
+
 int s21_sprintf(char *str, const char *format, ...);
-int choose_return_type(const char *format);
+int choose_return_type(const char *format, int *specifier_index);
 
 enum is_true{
     FALSE,
     TRUE
+};
+
+enum datatype{
+    CHAR,
+    INT
 };
 
 int main() {
@@ -36,7 +51,7 @@ int main() {
         printf("Memory could not be allocated");
     } else {
         char exclamation_point = '!';
-        sprintf(pointer_str_array, "Hello world%c!%c", exclamation_point, exclamation_point);
+        s21_sprintf(pointer_str_array, "Hello world%c!%c", exclamation_point, exclamation_point);
         puts(pointer_str_array);
     }
     free(pointer_str_array);
@@ -44,29 +59,35 @@ int main() {
     return 0;
 }
 
-int choose_return_type(const char *format) {
+int choose_return_type(const char *format, int *specifier_index) {
     int amount_of_specifiers = 15, is_true = FALSE;
     for (int i = 0; i < amount_of_specifiers; ++i) {
         if (specifiers[i] == *format) {
             is_true = TRUE;
+            printf("i = %d", i);
+            *specifier_index = i;
         }
     }
+    printf("specifier index in return choose = %d\n", *specifier_index);
     return is_true;
 }
 
 int s21_sprintf(char *str, const char *format, ...) {
-    // va_list is effictively a pointer to an arguments in the varargs array
+     // va_list is effictively a pointer to an arguments in the varargs array
     va_list argp;
     // After calling va_start argp ooints at the first vararg argument
     va_start(argp, format);
-    int realloc_counter = 2, specifier_counter = 0, index = 0;
+    int realloc_counter = 2, specifier_counter = 0, index = 0, specifier_index = -1;
         while (*format != '\0') {
             if (*format == '%') {
                 ++specifier_counter;
                 ++format;
                 // Replace that if with a function to keep specifier easy, short and simple
-                    if (choose_return_type(format)) {
+                // Add pointer to index of char specifier and call that index using an enum
+                // where each one is specific type of data
+                    if (choose_return_type(format, &specifier_index)) {
                     char char_to_print = va_arg(argp, int);
+                    printf("%d\n", specifier_index);
                     str[index] = char_to_print;
                     ++index;
                     continue;
