97555c5c791e335fd548856495f44846acb543f9
163114 <88505337+163114@users.noreply.github.com>
2022-04-30 14:58:38 +0300

    Added specifiers for sprintf and sscanf

---
+++ s21_string+/src/s21_specifiers.h
@@ -0,0 +1,8 @@
+#ifndef SPECIFIERS_H
+#define SPECIFIERS_H
+
+// Define the char array of specifiers to use in spritf and sscanf
+char specifiers[15] = {'c', 'd', 'i', 'e', 'E', 'f', 'g', 'G', 'o', 's',
+'u', 'x', 'X', 'p', 'n'};
+
+#endif
