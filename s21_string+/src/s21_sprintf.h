ba59152207d7f740942e332b066cbaf93b7d0f10
163114 <88505337+163114@users.noreply.github.com>
2022-05-01 16:27:25 +0300

    More decomposition

---
+++ s21_string+/src/s21_sprintf.h
@@ -0,0 +1,17 @@
+#ifndef S21_SPRINTF_H
+#define S21_SPRINTF_H
+
+#include <stdio.h>
+#include <stdlib.h>
+// The function is used in vararg arguments in the s21_printf function
+#include <stdarg.h>
+
+int s21_sprintf(char *str, const char *format, ...);
+int choose_return_type(const char *format, int *specifier_index);
+
+enum is_true{
+    FALSE,
+    TRUE
+};
+
+#endif
