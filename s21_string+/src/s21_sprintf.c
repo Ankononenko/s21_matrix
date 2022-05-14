2e441a991bcc4a1173f7be5662d9c3e55369a835
163114 <88505337+163114@users.noreply.github.com>
2022-05-14 12:01:16 +0300

    Update s21_sprintf.c

--- s21_string+/src/s21_sprintf.c
+++ s21_string+/src/s21_sprintf.c
@@ -33,7 +33,8 @@ int main() {
         // puts(pointer_str_array);
 
         // %d
-        int number_six = '6';
+        // ASCII table int 6 == char 54; I think I should do the following - if int -> value + 48 to convert to char for ASCII
+        int number_six = 15;
         s21_sprintf(pointer_str_array, "Hello world%d!%c\n", number_six, exclamation_point);
         puts(pointer_str_array);
 
@@ -69,14 +70,20 @@ int s21_sprintf(char *buffer, const char *format, ...) {
             if (*format == '%') {
                 ++format;
                 if (choose_return_type(format, &specifier_index)) {
+                    // Variable for char for string function
                     char char_to_print = '\0';
+                    // Variable for int
+                    int int_type_to_print = 0;
+                    // Pointer for strings and address
                     const char *pointer_char_to_print = &char_to_print;
-                    if (specifier_index == CHARACTER_C ||
-                        specifier_index == SIGNED_DECIMAL_INTEGER_D ||
-                        specifier_index == SIGNED_INTEGER_I) {
+                    if (specifier_index == CHARACTER_C) {
                         // va_arg takes the pointer and a type, 
                         // increments the pointer by that size and returns it as a char
-                        char_to_print = va_arg(argp, int); 
+                        int_type_to_print = va_arg(argp, int); 
+                    } else if (specifier_index == SIGNED_DECIMAL_INTEGER_D ||
+                        specifier_index == SIGNED_INTEGER_I) {
+                        int_type_to_print = va_arg(argp, int);
+                        int_type_to_print += 48;
                     } else if (specifier_index == DECIMAL_FLOATING_POINT_E ||
                         specifier_index == DECIMAL_FLOATING_POINT_E_CAPITAL ||
                         specifier_index == DECIMAL_FLOATING_POINT_F ||
@@ -94,8 +101,9 @@ int s21_sprintf(char *buffer, const char *format, ...) {
                     } else if (specifier_index == POINTER_ADDRESS_P) {
                          pointer_char_to_print = va_arg(argp, void*);
                          char_to_print = *pointer_char_to_print;
-                     } 
-                    buffer[index] = char_to_print;
+                    } 
+                    printf("Inside the while loop - %d\n", int_type_to_print);
+                    buffer[index] = ((char)int_type_to_print);
                     ++index;
                     ++format;
                     continue;
