e46f53438224d83b5cfb6b3407632ec31d331aee
163114 <88505337+163114@users.noreply.github.com>
2022-05-16 19:55:42 +0300

    Removed the base check
    Removed the base check because in my solution I use only 10-base. For 100% coverage need to comment that out

--- s21_string+/src/s21_itoa.c
+++ s21_string+/src/s21_itoa.c
@@ -1,50 +1,55 @@
 #include "s21_itoa.h"
 
-int main() {
-    int positive_number = 123456;  
-    char buffer[256];  
-    if (s21_itoa(positive_number, buffer, 10) != NULL) {  
-        printf("Input = %d, base = %d, buffer = %s\n", positive_number, 10, buffer);  
-    }  
-    int negative_number = -2310;  
-    if (s21_itoa(negative_number, buffer, 10) != NULL) {  
-        printf("Input = %d, base = %d, buffer = %s\n", negative_number, 10, buffer);  
-    }  
-    // Binary test
-    int number_for_binary = 10;  
-    if (s21_itoa(number_for_binary, buffer, 2) != NULL) {  
-        printf("Input = %d, base = %d, buffer = %s\n", number_for_binary, 2, buffer);  
-    }  
-    return 0;
-}
+// int main() {
+//     int positive_number = 123456;  
+//     char buffer[256];  
+//     if (s21_itoa(positive_number, buffer, 10) != NULL) {  
+//         printf("Input = %d, base = %d, buffer = %s\n", positive_number, 10, buffer);  
+//     }  
+//     int negative_number = -2310;  
+//     if (s21_itoa(negative_number, buffer, 10) != NULL) {  
+//         printf("Input = %d, base = %d, buffer = %s\n", negative_number, 10, buffer);  
+//     }  
+//     // Binary test
+//     int number_for_binary = 10;  
+//     if (s21_itoa(number_for_binary, buffer, 2) != NULL) {  
+//         printf("Input = %d, base = %d, buffer = %s\n", number_for_binary, 2, buffer);  
+//     }  
+//     return 0;
+// }
 
 char* s21_itoa(int number, char *buffer, int base) {
-    int current = 0;  
-    if (number == 0) {  
-        buffer[current++] = '0';  
-        buffer[current] = '\0';  
-        return buffer;  
-    }  
-    int num_digits = 0;  
-    if (number < 0) {  
-        if (base == 10) {  
-        ++num_digits;  
-        buffer[current] = '-';  
-        ++current;  
-        number *= -1;  
-    } else { 
-        return NULL;
-        }  
-    }  
-    num_digits += (int)floor(log(number) / log(base)) + 1;  
-    while (current < num_digits) {  
-        int base_val = (int) pow(base, num_digits -1 -current);  
-        int num_val = number / base_val;  
-        char value = num_val + '0';  
-        buffer[current] = value;  
-        ++current;  
-        number -= base_val * num_val;  
-    }  
-    buffer[current] = '\0';  
-    return buffer;  
+    int current = 0;
+    if (number == 0) {
+        buffer[current++] = '0';
+        buffer[current] = '\0';
+        return buffer;
+    }
+    int num_digits = 0;
+    if (number < 0) {
+        // I removed base check because in our case base in only one - 10
+        // We could bring it back if we need to work with other types of possbile base values - 2, 8, 16
+        // if (base == 10) { 
+        ++num_digits;
+        buffer[current] = '-';
+        ++current;
+        number *= -1;
+        // } 
+        // Used for the case when to the function is passed a non-valid radix argument
+        // Since in your case base in only one - 10, we don't need it
+        // else {
+        //     return NULL;
+        // }
+    }
+    num_digits += (int)floor(log(number) / log(base)) + 1;
+    while (current < num_digits) {
+        int base_val = (int) pow(base, num_digits -1 -current);
+        int num_val = number / base_val;
+        char value = num_val + '0';
+        buffer[current] = value;
+        ++current;
+        number -= base_val * num_val;
+    }
+    buffer[current] = '\0';
+    return buffer;
 }
