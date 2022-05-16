d18b4773b661bc6ac38f9201c4800330acda2738
163114 <88505337+163114@users.noreply.github.com>
2022-05-16 19:56:47 +0300

    Fixed the bugs with MIN and MAX range of INT

--- s21_string+/src/s21_sprintf.c
+++ s21_string+/src/s21_sprintf.c
@@ -7,13 +7,10 @@ What to return - number of characters written to buffer
 2. Formatting support:
     a. Specifiers: 
 +       c, 
-            To finish testing of %c I need to finish %d for. Want to test how works %c as %d and the other way around
-        d,
++            To finish testing of %c I need to finish %d for. Want to test how works %c as %d and the other way around
++        d,
 +           Itoa function
-            Put the argp number in the array of chars
-            Find the lenght of that array
-            Convert in to numbers
-            I need to scan the digits one at the time.
++           Put the argp in the array of chars and append to the buffer
         i, 
         f, 
         s, 
@@ -34,15 +31,47 @@ What to return - number of characters written to buffer
 
 #include "s21_sprintf.h"
 
-int main() {
-    char buffer[100];
-    // char exclamation_point = '!';
-    int number = 15;
-    // s21_sprintf(buffer, "Hello world%c!%c\n", exclamation_point, exclamation_point);
-    s21_sprintf(buffer, "Hello world%d!%d\n", number, number);
-    puts(buffer);
-    return 0;
-}
+// !!! THE COMMENTED CODE IS FOR TROUBLESHOOTING. DELETE THAT LATER
+// char* s21_itoa(int number, char *buffer, int base) {
+//     int current = 0;
+//     if (number == 0) {
+//         buffer[current++] = '0';
+//         buffer[current] = '\0';
+//         return buffer;
+//     }
+//     int num_digits = 0;
+//     if (number < 0) {
+//         if (base == 10) {
+//         ++num_digits;
+//         buffer[current] = '-';
+//         ++current;
+//         number *= -1;
+//     } else {
+//         return NULL;
+//         }
+//     }
+//     num_digits += (int)floor(log(number) / log(base)) + 1;
+//     while (current < num_digits) {
+//         int base_val = (int) pow(base, num_digits -1 -current);
+//         int num_val = number / base_val;
+//         char value = num_val + '0';
+//         buffer[current] = value;
+//         ++current;
+//         number -= base_val * num_val;
+//     }
+//     buffer[current] = '\0';
+//     return buffer;
+// }
+
+// int main() {
+//     char buffer[100];
+//     // char exclamation_point = '!';
+//     int number = 2147483647;
+//     // s21_sprintf(buffer, "Hello world%c!%c\n", exclamation_point, exclamation_point);
+//     s21_sprintf(buffer, "%d Hello world %d ! %d\n", number, number, number);
+//     puts(buffer);
+//     return 0;
+// }
 
 int s21_sprintf(char *buffer, const char *format, ...) {
     // va_list is effictively a pointer to an arguments in the varargs array
@@ -55,9 +84,8 @@ int s21_sprintf(char *buffer, const char *format, ...) {
             ++number_of_specifires;    
             ++format;
             // Here we can call another function to which we can pass variable arguments by passing a single va_list pointer
-            choose_return_type(buffer, format, index, argp);
+            choose_return_type(buffer, format, &index, argp);
             ++format;
-            ++index;
         } else {
             buffer[index] = *format;
             ++index;
@@ -69,7 +97,7 @@ int s21_sprintf(char *buffer, const char *format, ...) {
     return index;
 }
 
-void choose_return_type(char *buffer, const char *format, int index, va_list argp) {
+void choose_return_type(char *buffer, const char *format, int *index, va_list argp) {
     if ('c' == *format) {
         c_specifier(buffer, index, argp);
     }
@@ -78,17 +106,18 @@ void choose_return_type(char *buffer, const char *format, int index, va_list arg
     }
 }
 
-void c_specifier(char *buffer, int index, va_list argp) {
-    buffer[index] = va_arg(argp, int);
+void c_specifier(char *buffer, int *index, va_list argp) {
+    buffer[*index] = va_arg(argp, int);
+    ++*index;
 }
 
-void d_specifier(char *buffer, int index, va_list argp) {
-    char array_for_int[10];
-    itoa(va_arg(argp, int), array_for_int, 10);
-    printf("%s", array_for_int);
-
-    ++index;
-    buffer[index] = 'a';
+void d_specifier(char *buffer, int *index, va_list argp) {
+    char array_for_int[12];
+    int int_array_index = 0;
+    s21_itoa(va_arg(argp, int), array_for_int, 10);
+    while (array_for_int[int_array_index] != '\0') {
+        buffer[*index] = array_for_int[int_array_index];
+        ++int_array_index;
+        ++*index;
+    }
 }
-
-// int find_array_length
