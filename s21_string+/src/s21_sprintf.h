9dc9f5c1f083c41947a2578023eb6a26272b49f4
163114 <88505337+163114@users.noreply.github.com>
2022-05-21 19:03:20 +0300

    Final push for the day

--- s21_string+/src/s21_sprintf.h
+++ s21_string+/src/s21_sprintf.h
@@ -5,18 +5,22 @@
 #include <stdlib.h>
 // The function is used in vararg arguments in the s21_printf function
 #include <stdarg.h>
-// To remove later - waitning for Ilyusha to complete strcpy
-// Also string.h is imported for itoa. Later I need to make my own itoa, if it works as planned
-#include <string.h>
+#include <math.h>
+// Include the itoa to convert int to string 
+#include "s21_itoa.h"
+#include <assert.h>
 
 int s21_sprintf(char *buffer, const char *format, ...);
-void choose_return_type(char *buffer, const char *format, int index, va_list argp);
-void c_specifier(char *buffer, int index, va_list argp);
-void d_specifier(char *buffer, int index, va_list argp);
+void choose_return_type(char *buffer, const char *format, int *index, va_list argp);
+void c_specifier(char *buffer, int *index, va_list argp);
+void d_i_specifier(char *buffer, int *index, va_list argp);
+// void i_specifier(char *buffer, int *index, va_list argp); - Redundant. May need in the future. Going to save it for now just in case
+// int flip_to_array(int number_to_flip, char *pointer_to_array); - Redundant. May need in the future. Going to save it for now just in case
+void f_specifier(char *buffer, int *index, va_list argp);
 
-enum is_true{
-    FALSE,
-    TRUE
-};
+// enum is_true{
+//     FALSE,
+//     TRUE
+// };
 
-#endif
+#endif //  S21_SPRINTF_H
