cbb77319f65ec8caa658aac5a39253e1a3f22788
Finch Renda <a63.114@yandex.ru>
2022-07-29 16:46:43 +0300

    Final push for the day. I should go and sleep. To tired to be productive

--- cat_and_grep/src/grep/s21_grep.c
+++ cat_and_grep/src/grep/s21_grep.c
@@ -13,6 +13,24 @@ The resulting executable file must be placed in the directory src/grep/ and name
 #include "s21_grep.h"
 
 int main() {
-    printf("Test");
+    Flags_grep flags_grep = {0};
+    initialize_flags_grep(&flags_grep);
+    Data_grep data_grep = {0};
+    initialize_data_grep(&data_grep);
     return 0;
+}
+
+void initialize_data_grep(Data_grep* data_grep) {
+    memset(data_grep->all_flags_array, 0, TOTAL_NUM_FLAGS_GREP * sizeof(char));
+    memset(data_grep->all_filenames_array, 0, TOTAL_NUM_FILENAMES * sizeof(char));
+    data_grep->number_of_files = 0;   
+}
+
+void initialize_flags_grep(Flags_grep* flags_grep) {
+    flags_grep->e = FALSE;
+    flags_grep->i = FALSE;
+    flags_grep->v = FALSE;
+    flags_grep->c = FALSE;
+    flags_grep->l = FALSE;
+    flags_grep->n = FALSE;
 }
--- cat_and_grep/src/grep/s21_grep.h
+++ cat_and_grep/src/grep/s21_grep.h
@@ -3,6 +3,8 @@
 
 #define TOTAL_NUM_FLAGS_GREP 7
 #define MAX_LENGHT_OF_FLAG_GREP 5
+#define TOTAL_NUM_FILENAMES 50
+#define MAX_LENGHT_OF_FILENAME 30
 
 #include "../cat/s21_cat.h"
 
@@ -18,5 +20,16 @@ typedef struct Flags_grep {
     int l;
     int n;
 } Flags_grep;
+
+typedef struct Data_grep {
+    // An array to keep all of the flags valid and invalid to sort them out later
+    char all_flags_array[TOTAL_NUM_FLAGS_GREP][MAX_LENGHT_OF_FLAG_GREP];
+    // And an array for filenames. Valid and invalid. Going to sort them out later
+    char all_filenames_array[TOTAL_NUM_FILENAMES][MAX_LENGHT_OF_FILENAME];
+    int number_of_files;
+} Data_grep;
+
+void initialize_flags_grep(Flags_grep* flags_grep);
+void initialize_data_grep(Data_grep* data_grep);
 
 #endif  // SRC_S21_GREP_H
