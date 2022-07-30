5c8bf425374680fe88f4cc178e9503c653361910
Finch Renda <a63.114@yandex.ru>
2022-07-30 20:38:47 +0300

    Finished with -l flag

--- cat_and_grep/src/grep/s21_grep.c
+++ cat_and_grep/src/grep/s21_grep.c
@@ -19,30 +19,61 @@ int main(int argc, char *argv[]) {
     initialize_data(&data);
     if (check_start_conditions(argc, argv, &data)) {
         pass_flags_to_structure(&flags, &data);
-        print_result(/* &flags, */ &data);
+        print_result(&flags, &data);
     } else {
         fprintf(stderr, "Flags were not valid \n");
     }
     return 0;
 }
 
-void print_result(/*Flags const* flags, */ Data* data) {
+void print_result(Flags const* flags, Data* data) {
     for (int index_for_files = 0; index_for_files < data->number_of_files; ++index_for_files) {
         if (check_if_files_exist(index_for_files, data)) {
             FILE *file = fopen(data->all_filenames_array[index_for_files], "r");
             while (parse_line(file, data)) {
-                int index = 0;
-                while (data->line_array[index] != '\0') {
-                    printf("%c", data->line_array[index]);
-                    ++index;
+                // Here I pass a boolean value of print_line, change it to TRUE of FALSE
+                // Based on the current flags that are in power and print or not to print the line in the end
+                if (flags->e) {
+                    handle_e(data);
                 }
-                printf("\n");
+                if (flags->l) {
+                    handle_l(data);
+                }
+                // Print the line in the end if it was found
+                if (data->want_to_print_line) {
+                    printf("%s\n", data->line_array);
+                    data->want_to_print_line = FALSE;
+                }
             }
-            printf("EOF \n");
         } else {
             // Error message when a file doesn't exist
             fprintf(stderr, "File doesn't exist \n");
         }
+    }
+}
+
+void handle_l(Data* data) {
+    int line_lenght = 0, pattern_lenght = 0;
+    line_lenght = strlen(data->line_array);
+    pattern_lenght = strlen(data->pattern_array);
+    for (int index = 0; index <= line_lenght; ++ index) {
+        data->line_array[index] = tolower(data->line_array[index]);
+    }
+    for (int index = 0; index <= pattern_lenght; ++ index) {
+        data->pattern_array[index] = tolower(data->pattern_array[index]);
+    }
+    if (strstr(data->line_array, data->pattern_array)) {
+        data->want_to_print_line = TRUE;
+    }
+}
+
+void handle_e(Data* data) {
+    // Strstr function finds the first occurrence of the substring needle in the string haystack. 
+    // The terminating '\0' characters are not compared.
+    // Returns a pointer to the first occurrence in haystack
+    // or a null pointer if the sequence is not present in haystack
+    if (strstr(data->line_array, data->pattern_array)) {
+        data->want_to_print_line = TRUE;
     }
 }
 
@@ -64,13 +95,13 @@ int check_start_conditions(const int argc, char *argv[], Data* data) {
     // Condition to check if there are flags and text-files
     // Parse flags() should return TRUE or FALSE if the flags are valid or not
     // And the files exist
-    if (argc >= 4 && parse_flags_patters_filenames(/*argc, */argv, data)) {
+    if (argc >= 4 && parse_flags_patterns_filenames(/*argc, */argv, data)) {
         conditions = TRUE;
     }
     return conditions;
 }
 
-int parse_flags_patters_filenames(char *argv[], Data* data) {
+int parse_flags_patterns_filenames(char *argv[], Data* data) {
     int is_valid_input = FALSE;
     // Counters to know when to stop in iterating over the arrays
     int counter_for_flags = 0;
@@ -78,15 +109,14 @@ int parse_flags_patters_filenames(char *argv[], Data* data) {
     int element_index = 1;
     parse_flags(argv, data, &counter_for_flags, &element_index);
     // When first index of argv element != '-' read the first pattern (for -e -f) and then the rest may be continuation of flags and the filenames
-    strcpy(data->patters_array, argv[element_index]);
-    ++element_index;
+    // Write to the array of patters only if the pattern wasn't written previously
+    if (data->pattern_array[0] == 0) {
+        parse_pattern(data, argv, &element_index);
+    }
     // Parse flags in case of -e pattern -c filename
     parse_flags(argv, data, &counter_for_flags, &element_index);
-    while (argv[element_index]) {
-        strcpy(data->all_filenames_array[data->number_of_files], argv[element_index]);
-        ++data->number_of_files;
-        ++element_index;
-    }
+    // Parse filenames to the array of filenames
+    parse_filenames(data, argv, &element_index);
     if (check_if_flags_are_valid(counter_for_flags, data)) {
             is_valid_input = TRUE;
     }
@@ -144,11 +174,33 @@ int check_if_flags_are_valid(const int counter_for_flags, Data const* data) {
     return flags_are_valid;
 }
 
+void parse_pattern(Data* data, char *argv[], int* element_index) {
+    strcpy(data->pattern_array, argv[*element_index]);
+    ++*element_index;
+}
+
 void parse_flags(char *argv[], Data* data, int* counter_for_flags, int* element_index) {
-    int letter_index = 0;
+    int letter_index = 0, is_previous_e_flag = FALSE;
     while (argv[*element_index][letter_index] == '-') {
+        // When flag is -e and the pattern starts with '-', it should be read to the pattern array
+        if (!strcmp(argv[*element_index], "-e")) {
+            is_previous_e_flag = TRUE;
+        }
         strcpy(data->all_flags_array[*counter_for_flags], argv[*element_index]);
         ++*counter_for_flags;
+        ++*element_index;
+        // The pattern is being copied to the array of patterns in case the previous flag was -e
+        if (is_previous_e_flag) {
+            parse_pattern(data, argv, element_index);
+            is_previous_e_flag = FALSE;
+        }
+    }
+}
+
+void parse_filenames(Data* data, char *argv[], int* element_index) {
+    while (argv[*element_index]) {
+        strcpy(data->all_filenames_array[data->number_of_files], argv[*element_index]);
+        ++data->number_of_files;
         ++*element_index;
     }
 }
@@ -156,10 +208,11 @@ void parse_flags(char *argv[], Data* data, int* counter_for_flags, int* element_
 void initialize_data(Data* data) {
     memset(data->all_flags_array, 0, TOTAL_NUM_FLAGS * sizeof(char));
     memset(data->all_filenames_array, 0, TOTAL_NUM_FILENAMES * sizeof(char));
-    memset(data->patters_array, 0, MAX_LENGHT_OF_PATTERN * sizeof(char));
+    memset(data->pattern_array, 0, MAX_LENGHT_OF_PATTERN * sizeof(char));
     memset(data->line_array, 0, MAX_LENGHT_OF_LINE * sizeof(char));
     data->number_of_files = 0;
     data->newline = '\n';
+    data->want_to_print_line = FALSE;
 }
 
 void initialize_flags(Flags* flags) {
--- cat_and_grep/src/grep/s21_grep.h
+++ cat_and_grep/src/grep/s21_grep.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <string.h>
+// Used for tolower function
+#include <ctype.h>
 
 #define TOTAL_NUM_FLAGS 7
 #define MAX_LENGHT_OF_FLAG 5
@@ -35,22 +37,27 @@ typedef struct Data {
     // And an array for filenames. Valid and invalid. Going to sort them out later
     char all_filenames_array[TOTAL_NUM_FILENAMES][MAX_LENGHT_OF_FILENAME];
     // Array to store the patterns
-    char patters_array[MAX_LENGHT_OF_PATTERN];
+    char pattern_array[MAX_LENGHT_OF_PATTERN];
     // Array to hold the current line
     char line_array[MAX_LENGHT_OF_LINE];
     int number_of_files;
+    int want_to_print_line;
     char newline;
 } Data;
 
 void initialize_flags(Flags* flags);
 void initialize_data(Data* data);
 int check_start_conditions(const int argc, char *argv[], Data* data);
-int parse_flags_patters_filenames(char *argv[], Data* data);
+int parse_flags_patterns_filenames(char *argv[], Data* data);
 void parse_flags(char *argv[], Data* data, int* counter_for_flags, int* element_index);
+void parse_pattern(Data* data, char *argv[], int* element_index);
+void parse_filenames(Data* data, char *argv[], int* element_index);
 int check_if_flags_are_valid(const int counter_for_flags, Data const* data);
 int check_if_files_exist(const int filename_index, Data const* data);
 void pass_flags_to_structure(Flags* flags, Data const* data);
-void print_result(/*Flags const* flags, */ Data* data);
+void print_result(Flags const* flags, Data* data);
 int parse_line(FILE *file, Data* data);
+void handle_e(Data* data);
+void handle_l(Data* data);
 
 #endif  // SRC_S21_GREP_H
