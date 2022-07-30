1e94025df06e041c6705554586e3f984f653aa03
Finch Renda <a63.114@yandex.ru>
2022-07-30 15:41:10 +0300

    Done with the valid flags check

--- cat_and_grep/src/grep/s21_grep.c
+++ cat_and_grep/src/grep/s21_grep.c
@@ -12,25 +12,91 @@ The resulting executable file must be placed in the directory src/grep/ and name
 
 #include "s21_grep.h"
 
-int main() {
-    Flags_grep flags_grep = {0};
-    initialize_flags_grep(&flags_grep);
-    Data_grep data_grep = {0};
-    initialize_data_grep(&data_grep);
+int main(int argc, char *argv[]) {
+    Flags flags = {0};
+    initialize_flags(&flags);
+    Data data = {0};
+    initialize_data(&data);
+    if (check_start_conditions(argc, argv, &data)) {
+        printf("Success");
+    } else {
+        fprintf(stderr, "Flags were not valid \n");
+    }
     return 0;
 }
 
-void initialize_data_grep(Data_grep* data_grep) {
-    memset(data_grep->all_flags_array, 0, TOTAL_NUM_FLAGS_GREP * sizeof(char));
-    memset(data_grep->all_filenames_array, 0, TOTAL_NUM_FILENAMES * sizeof(char));
-    data_grep->number_of_files = 0;   
+int check_start_conditions(const int argc, char *argv[], Data* data) {
+    int conditions = FALSE;
+    // Condition to check if there are flags and text-files
+    // Parse flags() should return TRUE or FALSE if the flags are valid or not
+    // And the files exist
+    if (argc >= 4 && parse_flags_patters_filenames(/*argc, */argv, data)) {
+        conditions = TRUE;
+    }
+    return conditions;
 }
 
-void initialize_flags_grep(Flags_grep* flags_grep) {
-    flags_grep->e = FALSE;
-    flags_grep->i = FALSE;
-    flags_grep->v = FALSE;
-    flags_grep->c = FALSE;
-    flags_grep->l = FALSE;
-    flags_grep->n = FALSE;
+int parse_flags_patters_filenames(/*const int argc, */char *argv[], Data* data) {
+    int is_valid_input = FALSE;
+    // Counters to know when to stop in iterating over the arrays
+    int counter_for_flags = 0;
+    // Iterate over the argv to sort out flags, pattern and filenames
+    int element_index = 1;
+    parse_flags(argv, data, &counter_for_flags, &element_index);
+    // When first index of argv element != '-' read the first pattern (for -e -f) and then the rest may be continuation of flags and the filenames
+    strcpy(data->patters_array, argv[element_index]);
+    ++element_index;
+    // Parse flags in case of -e pattern -c filename
+    parse_flags(argv, data, &counter_for_flags, &element_index);
+    while (argv[element_index]) {
+        strcpy(data->all_filenames_array[data->number_of_files], argv[element_index]);
+        ++data->number_of_files;
+        ++element_index;
+    }
+    if (check_if_flags_are_valid(counter_for_flags, data)) {
+            is_valid_input = TRUE;
+    }
+    return is_valid_input;
+}
+
+int check_if_flags_are_valid(const int counter_for_flags, Data const* data) {
+    int flags_are_valid = FALSE, index_all_flags = 0, number_of_valid_flags = 0;
+    while (index_all_flags != counter_for_flags) {
+        for (int index_possible_flags = 0;
+        index_possible_flags < TOTAL_NUM_FLAGS; ++index_possible_flags) {
+            if (!strcmp(data->all_flags_array[index_all_flags], possible_flags[index_possible_flags])) {
+                ++number_of_valid_flags;
+            }
+        }
+        ++index_all_flags;
+    }
+    if (counter_for_flags == number_of_valid_flags) {
+        flags_are_valid = TRUE;
+    }
+    return flags_are_valid;
+}
+
+void parse_flags(char *argv[], Data* data, int* counter_for_flags, int* element_index) {
+    int letter_index = 0;
+    while (argv[*element_index][letter_index] == '-') {
+        strcpy(data->all_flags_array[*counter_for_flags], argv[*element_index]);
+        ++*counter_for_flags;
+        ++*element_index;
+    }
+}
+
+void initialize_data(Data* data) {
+    memset(data->all_flags_array, 0, TOTAL_NUM_FLAGS * sizeof(char));
+    memset(data->all_filenames_array, 0, TOTAL_NUM_FILENAMES * sizeof(char));
+    memset(data->patters_array, 0, MAX_LENGHT_OF_PATTERN * sizeof(char));
+    data->number_of_files = 0;   
+}
+
+void initialize_flags(Flags* flags) {
+    flags->e = FALSE;
+    flags->i = FALSE;
+    flags->v = FALSE;
+    flags->c = FALSE;
+    flags->l = FALSE;
+    flags->n = FALSE;
 }
--- cat_and_grep/src/grep/s21_grep.h
+++ cat_and_grep/src/grep/s21_grep.h
@@ -1,35 +1,48 @@
 #ifndef SRC_S21_GREP_H
 #define SRC_S21_GREP_H
 
-#define TOTAL_NUM_FLAGS_GREP 7
-#define MAX_LENGHT_OF_FLAG_GREP 5
+#include <stdio.h>
+#include <string.h>
+
+#define TOTAL_NUM_FLAGS 7
+#define MAX_LENGHT_OF_FLAG 5
 #define TOTAL_NUM_FILENAMES 50
 #define MAX_LENGHT_OF_FILENAME 30
+#define MAX_LENGHT_OF_PATTERN 30
 
-#include "../cat/s21_cat.h"
+enum true_of_false{
+    FALSE,
+    TRUE
+};
 
-static const char possible_flags_grep[TOTAL_NUM_FLAGS_GREP][MAX_LENGHT_OF_FLAG_GREP] = {
+static const char possible_flags[TOTAL_NUM_FLAGS][MAX_LENGHT_OF_FLAG] = {
     "-e", "-i", "-v", "-c", "-l", "-n", "NULL"
 };
 
-typedef struct Flags_grep {
+typedef struct Flags {
     int e;
     int i;
     int v;
     int c;
     int l;
     int n;
-} Flags_grep;
+} Flags;
 
-typedef struct Data_grep {
+typedef struct Data {
     // An array to keep all of the flags valid and invalid to sort them out later
-    char all_flags_array[TOTAL_NUM_FLAGS_GREP][MAX_LENGHT_OF_FLAG_GREP];
+    char all_flags_array[TOTAL_NUM_FLAGS][MAX_LENGHT_OF_FLAG];
     // And an array for filenames. Valid and invalid. Going to sort them out later
     char all_filenames_array[TOTAL_NUM_FILENAMES][MAX_LENGHT_OF_FILENAME];
+    // Array to store the patterns
+    char patters_array[MAX_LENGHT_OF_PATTERN];
     int number_of_files;
-} Data_grep;
+} Data;
 
-void initialize_flags_grep(Flags_grep* flags_grep);
-void initialize_data_grep(Data_grep* data_grep);
+void initialize_flags(Flags* flags);
+void initialize_data(Data* data);
+int check_start_conditions(const int argc, char *argv[], Data* data);
+int parse_flags_patters_filenames(/*const int argc, */char *argv[], Data* data);
+void parse_flags(char *argv[], Data* data, int* counter_for_flags, int* element_index);
+int check_if_flags_are_valid(const int counter_for_flags, Data const* data);
 
 #endif  // SRC_S21_GREP_H
