b5d59b32f14099ca8873d6fa14ba3ba56f82e186
Anton <88505337+finchren@users.noreply.github.com>
2022-10-25 18:56:11 +0300

    Almost finished with the second part. Need to save to a file

--- linux_monitoring_v1/src/02/main.sh
+++ linux_monitoring_v1/src/02/main.sh
@@ -25,3 +25,27 @@ echo "UPTIME_SEC = $uptime"
 
 # Display the IP of the local machine
 echo "IP = $(hostname -I)"
+
+# Get the network mask
+echo "MASK = $( ifconfig | grep -m2 'inet '| awk '{ if (NR==2) printf $4}' )"
+
+# Display the default gateway
+echo "GATEWAY = $( ip n | awk '{print $1 }' )"
+
+# Display the total RAM in GB with three decimal places
+echo "RAM_TOTAL = $( free -m | awk 'sub(/Mem:/,""){printf("%.3f\n",$1 / 1024) }' ) GB"
+
+# Display the used RAM in GBs with three decimal places
+echo "RAM_USED = $( free -m | awk 'sub(/Mem:/,""){printf("%.3f\n",$2 /1024) }' ) GB"
+
+# Display the free RAM in GBs with three decimal places
+echo "RAM_FREE = $( free -m | awk 'sub(/Mem:/,""){printf("%.3f\n",$3 / 1024) }' ) GB"
+
+# Display the size of the root partion in MBs with two decimal spaces
+echo "SPACE_ROOT = $( df -hk / | awk 'sub(/dev/,""){printf("%.2f\n",$2 / 1024) }' ) MB"
+
+# Display the used space of the root partion in MBs with two decimal places
+echo "SPACE_ROOT_USED = $( df -hk / | awk 'sub(/dev/,""){printf("%.2f",$3 / 1024) }' ) MB"
+
+# Display the free space of the root partion in MBs with two decimal places
+echo "SPACE_ROOT_FREE = $( df -hk / | awk 'sub(/dev/,""){printf("%.2f",$4 / 1024) }' ) MB"
