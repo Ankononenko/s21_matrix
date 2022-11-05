51d4a94b7e545bb04078e8ee6766ac8dd13409ec
Anton <88505337+finchren@users.noreply.github.com>
2022-11-05 18:59:33 +0300

    Finished with the 5th task

--- linux_monitoring_v1/src/05/main.sh
+++ linux_monitoring_v1/src/05/main.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+start=`date +%s.%N`
+
 . ./check_input
 . ./display_info
+
+# Execution time of the script
+end=`date +%s.%N`
+total_time=$(echo "$end-$start" | bc -l)
+format_total_time=$(printf "%0.1lf" $total_time)
+echo "Script execution time (in seconds) = $format_total_time"
