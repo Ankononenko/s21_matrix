75a172b9e5187e8210855bf27ec9b673332f8f68
Anton <88505337+finchren@users.noreply.github.com>
2022-11-15 19:39:14 +0300

    Almost finished with the first part of the project

--- linux_monitoring_v2/src/01/main.sh
+++ linux_monitoring_v2/src/01/main.sh
@@ -1,5 +1,19 @@
 #!/bin/bash/
 
 . ./check_input
+. ./check_empty_space
+. ./create_folders
 
 check_input $@
+
+
+free_space=$(check_empty_space)
+if [[ $free_space == "Enough free space" ]]; then
+
+	num_folders=$2
+	folder_names=$3
+	create_folders $num_folders $folder_names
+else
+	echo "There isn't enough free space left"
+	echo "Should be over 1GB left to run the script"
+fi
