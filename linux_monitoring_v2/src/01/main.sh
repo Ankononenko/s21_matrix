e76dbc18b6b6e12e06a1c2b82c0ac6702a278f0a
Anton <88505337+finchren@users.noreply.github.com>
2022-11-16 20:55:52 +0300

    Reworked create folders function a bit

--- linux_monitoring_v2/src/01/main.sh
+++ linux_monitoring_v2/src/01/main.sh
@@ -10,9 +10,10 @@ check_input $@
 free_space=$(check_empty_space)
 if [[ $free_space == "Enough free space" ]]; then
 
+	path=$1
 	num_folders=$2
 	folder_names=$3
-	create_folders $num_folders $folder_names
+	create_folders $path $num_folders $folder_names
 else
 	echo "There isn't enough free space left"
 	echo "Should be over 1GB left to run the script"
