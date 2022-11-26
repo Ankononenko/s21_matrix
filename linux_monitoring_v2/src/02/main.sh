952ee05fbfea0992a8308c2fc466d21d045e2732
Anthony <88505337+finchren@users.noreply.github.com>
2022-11-26 21:33:48 +0300

    Need to fix the bug when the files get created only in the first folder

--- linux_monitoring_v2/src/02/main.sh
+++ linux_monitoring_v2/src/02/main.sh
@@ -9,15 +9,19 @@ check_input $@
 
 
 free_space=$(check_empty_space)
-if [[ $free_space == "Enough free space" ]]; then
+path=test
+folder_names=$1
+file_names=$2
+file_size=$3
 
-	path=test
-	folder_names=$1
-	#create_folders $path $folder_names
-	file_names=$2
-	file_size=$3
+#if [[ $free_space == "Enough free space" ]]; then
+while [[ $free_space == "Enough free space" ]]
+do
+	create_folders $path $folder_names
 	create_files $path $file_names $file_size
-else
+done
+
+if ! [[ $free_space == "Enough free space" ]]; then
 	echo "There isn't enough free space left"
 	echo "Should be over 1GB left to run the script"
 fi
