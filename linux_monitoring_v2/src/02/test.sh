66ca3398edc99ef4021210db6f716132dcee6326
Anthony <88505337+finchren@users.noreply.github.com>
2022-11-28 21:10:03 +0300

    All works in the test directory. Now I need to switch the working directory to the one in the task

--- linux_monitoring_v2/src/02/test.sh
+++ linux_monitoring_v2/src/02/test.sh
@@ -16,27 +16,34 @@
 #	echo $dir
 #done
 
-num_files=3
-created_files=0
-file_size=12
-path=test
-extension="exe"
-dirs=$(find $path -type d -name '*271122*')
-for dir in $dirs; do
-	echo "Before if $num_files"
-	if [[ $created_files -le $num_files ]]; then
-		file_name=""
-		for (( index=0; index<counter_to_repeat; ++index )); do
-			file_name+="a"
-		done
-		((++counter_to_repeat))
-		file_name+="_"
-		file_name+=$(date +"%d%m%y")
-		fallocate -l $file_size"MB" $dir/$file_name"."$extension
-		((++created_files))
-	else
-		echo "In else before $num_files"
-		created_files=0
-		echo "In else after $num_files"
-	fi
-done
+#num_files=3
+#created_files=0
+#file_size=12
+#path=test
+#extension="exe"
+#dirs=$(find $path -type d -name '*271122*')
+#for dir in $dirs; do
+#	echo "Before if $num_files"
+#	if [[ $created_files -le $num_files ]]; then
+#		file_name=""
+#		for (( index=0; index<counter_to_repeat; ++index )); do
+#			file_name+="a"
+#		done
+#		((++counter_to_repeat))
+#		file_name+="_"
+#		file_name+=$(date +"%d%m%y")
+#		fallocate -l $file_size"MB" $dir/$file_name"."$extension
+#		((++created_files))
+#	else
+#		echo "In else before $num_files"
+#		created_files=0
+#		echo "In else after $num_files"
+#	fi
+#done
+
+
+full=$(basename -- "$1")
+without_kb="${full%k*}"
+echo "full - $full"
+echo "without kb - $without_kb"
+
