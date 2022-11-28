5796aa68cd5f9f38b91bcab272852383999b6e45
Anthony <88505337+finchren@users.noreply.github.com>
2022-11-28 21:10:23 +0300

    All works in the test directory. Now I need to switch the working directory to the one in the task

--- linux_monitoring_v2/src/02/main.sh
+++ linux_monitoring_v2/src/02/main.sh
@@ -5,6 +5,10 @@
 . ./create_folders
 . ./create_files
 
+start_time_to_display=`date +%T`
+echo "Start time of the script: $start_time_to_display" >> log_file.log
+start_time_to_count=`date +%s`
+
 check_input $@
 
 
@@ -21,3 +25,13 @@ if ! [[ $free_space == "Enough free space" ]]; then
 	echo "There isn't enough free space left"
 	echo "Should be over 1GB left to run the script"
 fi
+
+end_time_to_display=`date +%T`
+echo "End time of the script: $end_time_to_display" >> log_file.log
+end_time_to_count=`date +%s`
+runtime=$((end_time_to_count-start_time_to_count))
+echo "Run time of the script: $runtime seconds" >> log_file.log
+
+echo "Start time = $start_time_to_display"
+echo "End time = $end_time_to_display"
+echo "Runtime = $runtime seconds"
