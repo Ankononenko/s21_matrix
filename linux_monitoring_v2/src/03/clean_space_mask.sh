64926c979558e52391c7c0d12bca4b84f80ec8e7
Anton <88505337+finchren@users.noreply.github.com>
2022-11-30 15:05:31 +0300

    Finished with the third task

--- linux_monitoring_v2/src/03/clean_space_mask.sh
+++ linux_monitoring_v2/src/03/clean_space_mask.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 current_date=$(date +"%d%m%y")
-find ~ -path "*$current_date*" -delete
-find ~ -name "*$current_date*" -delete
+find ../02/test -path "*$current_date*" -delete
+find ../02/test -name "*$current_date*" -delete
