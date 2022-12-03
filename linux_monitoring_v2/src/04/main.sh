b991210a387f0a796af79ce66b0d9192c9fb2c5b
Anton <88505337+finchren@users.noreply.github.com>
2022-12-03 23:05:42 +0300

    Finished with the 4 part. Need to test it again tomorrow to be sure that it works correctly

--- linux_monitoring_v2/src/04/main.sh
+++ linux_monitoring_v2/src/04/main.sh
@@ -2,6 +2,11 @@
 
 . ./generate_date_and_time
 . ./generate_ip
+. ./generate_method
+. ./generate_username
+. ./generate_response_code
+. ./generate_bytes
+. ./generate_user_agent
 
 #generate_date_and_time
 #generate_ip
@@ -23,8 +28,17 @@ for (( repetitions = 0; repetitions <= $number_of_lines; ++repetitions )); do
 	second_s=$(insert_zero $second)
 
 	ip=$(generate_ip)
-	
-	echo "$year-$month-$day $hour_s:$minute_s:$second_s $ip"
+	method=$(generate_method)
+	username=$(generate_username)
+	response_code=$(generate_response_code)
+	bytes_sent=$(generate_bytes)
+	# HTTP referer is the name of an optional HTTP header field that identifies the address of the web page, from which the resource has been requested
+	http_referer="-"
+	user_agent=$(generate_user_agent)
+#	Below is old incorrect format that I've got from running nginx on Docker	
+#	echo "$year-$month-$day $hour_s:$minute_s:$second_s $ip"
+#	New log format from nginx.org manual. This one is the combined format
+	echo "$ip - $username - [$day/$month_s/$year:$hour_s:$minute_s:$second_s +0000] \"$method\" $response_code $bytes_sent $http_referer $user_agent"
 	max_step_for_the_clock=60
 	second=$(( $RANDOM % $max_step_for_the_clock + 1 + $second ))
 	if [[ $second -gt $max_second ]]; then
