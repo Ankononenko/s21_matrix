fa0cc3f7b0cecc828f737c9f822d057a0ab29b33
Anton <88505337+finchren@users.noreply.github.com>
2022-12-04 20:57:15 +0300

    Got unique IPs from the logs, got all requests with errros

--- linux_monitoring_v2/src/05/main.sh
+++ linux_monitoring_v2/src/05/main.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 . ./sort_by_response_code
+. ./get_unique_ips
+. ./get_errors
 
 regex="../04/[0-9]{2}-[0-9]{2}-[0-9]{4}.log"
+
+# 1. All entries sorted by response code
+# 2. All unique IPs found in the entries
+# 3. All requests with errors (resonse code - 4xx or 5xx)
+# 4. All unique IPs found among the erroneous requests
 
 choice=$1
 
@@ -10,17 +17,46 @@ case $choice in
 
 	1)
 		name_of_dir="sorted_by_response_code"
+		echo "The sorted data is going to be created in the \"$name_of_dir\" directory"
 		mkdir $name_of_dir
 
 		for dir in ../04/*; do
 			if [[ $dir =~ $regex ]]; then
 			sort_by_response_code $dir $name_of_dir
 		fi
-	done
+		done
+
 	;;
 
 	2)
-		echo "Second option"
+		name_of_dir="lists_of_unique_ips"
+		echo "The lists with the unique IPs are going to get created in the \"$name_of_dir\" directory"
+		mkdir $name_of_dir
+
+		for dir in ../04/*; do
+			if [[ $dir =~ $regex ]]; then
+			get_unique_ips $dir $name_of_dir
+		fi
+		done
+
+	;;
+
+	3)
+
+		name_of_dir="reqests_with_errors"
+		echo "The with errors are going to get created in the \"$name_of_dir\" directory"
+		mkdir $name_of_dir
+
+		for dir in ../04/*; do
+			if [[ $dir =~ $regex ]]; then
+			get_errors $dir $name_of_dir
+		fi
+		done
+
+	;;
+	
+	4)
+		echo "Forth option"
 	;;
 
 esac	
