43b2d3cf1e16474f36c30aa1dc8a23b6e03d84dd
Anton <88505337+finchren@users.noreply.github.com>
2022-12-17 21:56:39 +0300

    Finished with scraping of the information. Now need to pass it to Prometheus

--- linux_monitoring_v2/src/09/scrape_free_mem.sh
+++ linux_monitoring_v2/src/09/scrape_free_mem.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 function scrape_free_mem {
-	echo "free_mem_in_bytes $(free -b | awk 'NR==2{print $4}')" >> metrics.html
+	echo "# HELP free_mem_in_bytes The free RAM left in bytes" >> /var/www/html/index.html
+#	echo "# HELP free_mem_in_bytes The free RAM left in bytes" >> metrics.html
+	echo "# TYPE free_mem_in_bytes gauge" >> /var/www/html/index.html
+#	echo "# TYPE free_mem_in_bytes gauge" >> metrics.html
+	echo "free_mem_in_bytes $(free -b | awk 'NR==2{print $4}')" >> /var/www/html/index.html
+#	echo "free_mem_in_bytes $(free -b | awk 'NR==2{print $4}')" >> metrics.html
 }
