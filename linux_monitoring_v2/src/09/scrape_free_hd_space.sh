57999bf51d7ce7445fffdc1dde130de985280796
Anton <88505337+finchren@users.noreply.github.com>
2022-12-17 21:56:13 +0300

    Finished with scraping of the information. Now need to pass it to Prometheus

--- linux_monitoring_v2/src/09/scrape_free_hd_space.sh
+++ linux_monitoring_v2/src/09/scrape_free_hd_space.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 function scrape_free_hd_space {
-	echo "free_hd_space_in_bytes $(df -B1 /home | awk 'NR==2{print $4}')" >> metrics.html
+	echo "# HELP free_hd_space_in_bytes The total number of bytes free in home directory" >> /var/www/html/index.html
+#	echo "# HELP free_hd_space_in_bytes The total number of bytes free in home directory" >> metrics.html
+	echo "# TYPE free_hd_space_in_bytes gauge" >> /var/www/html/index.html
+#	echo "# TYPE free_hd_space_in_bytes gauge" >> metrics.html
+	echo "free_hd_space_in_bytes $(df -B1 /home | awk 'NR==2{print $4}')" >> /var/www/html/index.html
+#	echo "free_hd_space_in_bytes $(df -B1 /home | awk 'NR==2{print $4}')" >> metrics.html
 }
