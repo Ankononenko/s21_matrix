0522ddbf749e05c4ed6190df05faf76e32946473
Anton <88505337+finchren@users.noreply.github.com>
2022-12-17 21:57:03 +0300

    Finished with scraping of the information. Now need to pass it to Prometheus

--- linux_monitoring_v2/src/09/scrape_cpu.sh
+++ linux_monitoring_v2/src/09/scrape_cpu.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 function scrape_cpu {
-	sum=0
-	echo "cpu_used_in_percent $(ps -eo pcpu | awk '{s+=$1} END {print s}')" >> metrics.html
+	echo "# HELP cpu_used_in_percent The total CPU used as percent value" >> /var/www/html/index.html
+# Output to the 09 folder for testing
+#	echo "# HELP cpu_used_in_percent The total CPU used as percent value" >> metrics.html
+	echo "# TYPE cpu_used_in_percent gauge" >> /var/www/html/index.html
+#	echo "# TYPE cpu_used_in_percent gauge" >> metrics.html
+	echo "cpu_used_in_percent $(ps -eo pcpu | awk '{s+=$1} END {print s}')" >> /var/www/html/index.html
+#	echo "cpu_used_in_percent $(ps -eo pcpu | awk '{s+=$1} END {print s}')" >> metrics.html
 }
