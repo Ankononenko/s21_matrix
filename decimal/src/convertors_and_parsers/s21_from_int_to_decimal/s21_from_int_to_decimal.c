633b140a519cd569731065d2eca1db9c6346e1ab
Anton <a63.114@yandex.ru>
2023-02-05 20:33:48 +0300

    Remove the commented code

--- decimal/src/convertors_and_parsers/s21_from_int_to_decimal/s21_from_int_to_decimal.c
+++ decimal/src/convertors_and_parsers/s21_from_int_to_decimal/s21_from_int_to_decimal.c
@@ -1,22 +1,5 @@
 #include "s21_from_int_to_decimal.h"
 
-// int main() {
-//   // int one = 1;
-//   // printf("one = ");
-//   // num_to_binary(one);
-//   // int five = 5;
-//   // printf("\nfive = ");
-//   // num_to_binary(five);
-//   // int five_five = 555;
-//   // printf("\n555 = ");
-//   // num_to_binary(five_five);
-//   // int max_int = 2147483647;
-//   // printf("\nmax_int = ");
-//   // num_to_binary(max_int);
-
-//   return 0;
-// }
-
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
   int return_value = SUCCESS;
   if (dst) {
