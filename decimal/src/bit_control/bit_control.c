36dd87e98628c7ffd79bcda97d96794c597eeaf6
Anton <a63.114@yandex.ru>
2023-02-12 21:41:02 +0300

    Added macro versions of the functions - looks cleaner

--- decimal/src/bit_control/bit_control.c
+++ decimal/src/bit_control/bit_control.c
@@ -4,10 +4,15 @@ int is_bit_set(int num, int index) {
   return (num & (1 << index)) != 0;
 }
 
+void new_set_bit(s21_decimal* num, int bit, int index) {
+  (bit == 1) ? (num->bits[index / 32] |= (1 << index % 32)) : (num->bits[index / 32] &= (1 << index % 32));
+}
+
 int set_bit(int* num, int index) {
   (*num) |= (1 << index);
   return *num;
 }
+
 
 int reset_bit (int* num, int index) {
   (*num) = (*num) & ~(1 << index);
--- decimal/src/bit_control/bit_control.h
+++ decimal/src/bit_control/bit_control.h
@@ -1,12 +1,19 @@
 #ifndef BIT_CONTROL
 #define BIT_CONTROL
 
+#include "../s21_decimal.h"
+
+// New version of bit control functions. Macros look a lot cleaner
+#define SET_BIT(X, BIT, POS) (X |= (BIT << POS))
+#define ADD_BIT(X, POS) ((X) |= (1U << (POS)))
+
 typedef enum Bool { 
     FALSE = 0, 
     TRUE = 1,
 } Bool;
 
 int is_bit_set(int num, int index);
+void new_set_bit(s21_decimal* num, int bit, int index);
 int set_bit(int* num, int index);
 int reset_bit(int* num, int index);
 Bool place_bit(int* num, int index, Bool bit);
